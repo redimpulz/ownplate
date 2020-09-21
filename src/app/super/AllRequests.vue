3258048fd92b7ff3208ee1abc81910f9156568b0
Ken Takahashi <takaken@redimpulz.com>
2020-09-21 12:11:27 +0900

    コードフォーマット時の不具合箇所を修正

--- src/app/super/AllRequests.vue
+++ src/app/super/AllRequests.vue
@@ -9,51 +9,51 @@
         <td>Pub</td>
         <td>Delete</td>
         <td>Status</td>
-      </tr></tr>
-      v-for="request in requests"
-      :key="request.id"
-      >
-      <td style="width: 50%">
-        <router-link :to="`/r/${request.id}`">
-          {{ (restaurantsObj[request.id] || {}).restaurantName }}
-        </router-link>
-      </td>
-      <td>
-        {{ (restaurantsObj[request.id] || {}).onTheList ? "o":"-" }}
-      </td>
-      <td>
-        {{ (restaurantsObj[request.id] || {}).publicFlag ? "o":"-" }}
-      </td>
-      <td>
-        {{ (restaurantsObj[request.id] || {}).deletedFlag ? "o":"-" }}
-      </td>
-      <td>
-        <span>
-          {{ request.status == 1 ? "request" : "" }}
-        </span>
-      </td>
-      <td>
-        <span
-          v-if="
-            !restaurantsObj[request.id] ||
-              !restaurantsObj[request.id].onTheList
-          "
-          <b-button @click="enableList(restaurantsObj[request.id].id)"
-        >Enable</b-button>
-        </span>
-        <span v-else>
-          On the list
-        </span>
-      </td>
+      </tr>
+      <tr v-for="request in requests" :key="request.id">
+        <td style="width: 50%">
+          <router-link :to="`/r/${request.id}`">
+            {{ (restaurantsObj[request.id] || {}).restaurantName }}
+          </router-link>
+        </td>
+        <td>
+          {{ (restaurantsObj[request.id] || {}).onTheList ? "o" : "-" }}
+        </td>
+        <td>
+          {{ (restaurantsObj[request.id] || {}).publicFlag ? "o" : "-" }}
+        </td>
+        <td>
+          {{ (restaurantsObj[request.id] || {}).deletedFlag ? "o" : "-" }}
+        </td>
+        <td>
+          <span>
+            {{ request.status == 1 ? "request" : "" }}
+          </span>
+        </td>
+        <td>
+          <span
+            v-if="
+              !restaurantsObj[request.id] ||
+                !restaurantsObj[request.id].onTheList
+            "
+          >
+            <b-button @click="enableList(restaurantsObj[request.id].id)">
+              Enable
+            </b-button>
+          </span>
+          <span v-else>
+            On the list
+          </span>
+        </td>
       </tr>
     </table>
   </section>
 </template>
 
 <script>
-import BackButton from '~/components/BackButton';
-import { db } from '~/plugins/firebase.js';
-import * as firebase from 'firebase/app';
+import BackButton from "~/components/BackButton";
+import { db } from "~/plugins/firebase.js";
+import * as firebase from "firebase/app";
 
 export default {
   components: {
@@ -68,27 +68,27 @@ export default {
   },
   async mounted() {
     if (!this.$store.state.user || this.$store.getters.isNotSuperAdmin) {
-      this.$router.push('/');
+      this.$router.push("/");
     }
   },
   created() {
     this.detatcher = db
-      .collection('requestList')
+      .collection("requestList")
       .limit(100)
-      .orderBy('created', 'desc')
+      .orderBy("created", "desc")
       .onSnapshot(async snapshot => {
-        this.requests = snapshot.docs.map(this.doc2data('request'));
+        this.requests = snapshot.docs.map(this.doc2data("request"));
         const ids = this.requests.map(a => a.id);
         this.arrayChunk(ids, 10).map(async arr => {
           const resCols = await db
-            .collection('restaurants')
-            .where(firebase.firestore.FieldPath.documentId(), 'in', arr)
+            .collection("restaurants")
+            .where(firebase.firestore.FieldPath.documentId(), "in", arr)
             .get();
           if (!resCols.empty) {
             this.restaurantsObj = Object.assign(
               {},
               this.restaurantsObj,
-              this.array2obj(resCols.docs.map(this.doc2data('restaurant')))
+              this.array2obj(resCols.docs.map(this.doc2data("restaurant")))
             );
           }
         });
@@ -107,7 +107,7 @@ export default {
       }, []);
     },
     enableList(id) {
-      db.doc(`restaurants/${id}`).update('onTheList', true);
+      db.doc(`restaurants/${id}`).update("onTheList", true);
       const tmp = Object.assign({}, this.restaurantsObj);
       tmp[id].onTheList = true;
       this.restaurantsObj = tmp;
